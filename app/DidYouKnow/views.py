f06a0066fec6b4a45a4f610023f335ad4a4656c4
Josie Fessey <07jreckn@gmail.com>
2024-09-07 10:50:19 +0200

    sdfg

--- app/DidYouKnow/views.py
+++ app/DidYouKnow/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from models import Didyouknow
+from serializers import DidyouknowSerializer
 
-# Create your views here.
+class DidyouknowViewSet(viewsets.ModelViewSet):
+    queryset = Didyouknow.objects.all()
+    serializer_class = DidyouknowSerializer
+
