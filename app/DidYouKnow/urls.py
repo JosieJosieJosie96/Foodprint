cd25f641d270ecc2328b08a9249b693a0c46135f
JosieJosieJosie96 <157616116+JosieJosieJosie96@users.noreply.github.com>
2024-09-11 16:36:48 +0200

    update

--- app/DidYouKnow/urls.py
+++ app/DidYouKnow/urls.py
@@ -14,41 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, re_path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LandingPageViewSet
 
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Foodprint API",
-        default_version='v1',
-        description="API documentation for Foodprint application",
-
-        contact=openapi.Contact(email="contact@foodprint.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
+router = DefaultRouter()
+router.register(r'landingpages', LandingPageViewSet)
 
 urlpatterns = [
-    # Your other URL patterns...
-    path('admin/', admin.site.urls),  # Admin panel route
-
-    # Include your app's API routes
-    # path('api/', include('app.urls')),
-    # Swagger UI:
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-    # ReDoc UI:
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-    # Plain JSON and YAML endpoints:
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', include(router.urls)),
 ]
 
