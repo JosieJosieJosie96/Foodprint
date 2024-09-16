0bf0fa20be07936d4bd11af8e84c2b5df078b746
JosieJosieJosie96 <157616116+JosieJosieJosie96@users.noreply.github.com>
2024-09-16 13:23:15 +0200

    Backend sql

--- app/DidYouKnow/urls.py
+++ app/DidYouKnow/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import LandingPageViewSet
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'landingpages', LandingPageViewSet)
+
+
 
 urlpatterns = [
-    path('', include(router.urls)),
+
 ]
 
