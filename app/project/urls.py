6e750b029ceed69bf4ced213b61d5346665583a4
JosieJosieJosie96 <157616116+JosieJosieJosie96@users.noreply.github.com>
2024-09-17 17:01:04 +0200

    Started making QR code functionality

--- app/project/urls.py
+++ app/project/urls.py
@@ -19,7 +19,6 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     # Your other URL patterns...
     path('admin/', admin.site.urls),  # Admin panel route
